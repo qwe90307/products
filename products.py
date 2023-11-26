fe2ffb81e6bcb857df9f705f6796174520e95c33
Jia xuan <qwe90307@gmail.com>
2023-11-26 16:43:33 +0800

    cheak file

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
+import os # operating system
+
 # 讀取檔案
 products = []
-
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
+# 檢查檔案存在與否
+if os.path.isfile('products.csv'):
+	print('有此檔案')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+else:
+	print('查無此檔案')
 
 #讓使用者輸入
 while True:
@@ -18,7 +24,9 @@ while True:
 	products.append([name, price]) 
 print(products)
 
-#印出商品紀錄
+for p in products:
+	print(p[0], '的購買價格是', p [1])
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
